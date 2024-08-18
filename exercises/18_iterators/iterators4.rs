e9bc4dfd6fc488798c95a66e3d3dad22f95293d9
Nikola Damjanovic <iamnikoladamjanovic@gmail.com>
2024-08-18 12:43:47 +0200

    lifetime and iteratorsr

--- exercises/18_iterators/iterators4.rs
+++ exercises/18_iterators/iterators4.rs
@@ -3,9 +3,8 @@
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
-pub fn factorial(num: u64) -> u64 {
+pub fn factorial(mut num: u64) -> u64 {
     // Complete this function to return the factorial of num
     // Do not use:
     // - early returns (using the `return` keyword explicitly)
@@ -15,6 +14,8 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+
+    (2..=num).product()
 }
 
 #[cfg(test)]
